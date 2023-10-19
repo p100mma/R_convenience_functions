a1052c66b7fa286aafe0ff25af88a4e21a337fba
Thomas The Tank Engine <no@such_email.com>
2023-10-19 14:27:25 +0200

    fixed the inequalities in example sign function

--- basic_algorithms.R
+++ basic_algorithms.R
@@ -19,19 +19,19 @@ return(surrogateValue)
 }
 
 example_signFunction<- function(LV, m, some_constant, some_array){ #LV should contain nonegative min2max sorted elements, m is in 1:length(LV)
-    #find m for which sum_m >= some_constant and sum_pm < some_constant (previous m, that is m -1)
+    #find m for which sum_m <= some_constant and sum_pm > some_constant (previous m, that is m -1)
     array_thr.m<- some_array
     array_thr.m[ some_array < LV[[m]] ] = 0
     sum_m <- sum(array_thr.m)
-    if (sum_m < some_constant) 
+    if (sum_m > some_constant) 
         return(-1)
-    if ( (m==1) && ( sum_m >= some_constant) ) #special case, if there is no previous m, condition on m only suffices that m is a solution
+    if ( (m==1) && ( sum_m <= some_constant) ) #special case, if there is no previous m, condition on m only suffices that m is a solution
         return(0)
     array_thr.pm<- some_array  #pm <- previous m
     array_thr.pm[ some_array < LV[[m-1]] ] = 0
     sum_pm <- sum(array_thr.pm)
-    if ( ( sum_m >= some_constant) && (sum_pm >= some_constant) )  
+    if ( ( sum_m <= some_constant) && (sum_pm <= some_constant) )  
         return(1)
-    if ( ( sum_m >= some_constant) && (sum_pm < some_constant) ) #only possible other outcome if all LV are positive nad LV is sorted 
+    if ( ( sum_m <= some_constant) && (sum_pm > some_constant) ) #only possible other outcome if all LV are positive nad LV is sorted 
         return(0)                                                # but lets be verbose about it
 }
