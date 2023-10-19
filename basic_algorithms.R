629bc24138b8763768999f0aec5927b8ab973424
Thomas The Tank Engine <no@such_email.com>
2023-10-19 12:24:51 +0200

    minor change to example sign function- compute sum of some_array supplied by another argument to sing_function, not of vector from which we pick m-th element from

--- basic_algorithms.R
+++ basic_algorithms.R
@@ -18,18 +18,18 @@ if (sgnM==sgnL) L <- M else R <- M
 return(surrogateValue)
 }
 
-example_signFunction<- function(LV, m, some_constant){ #LV should contain nonegative min2max sorted elements, m is in 1:length(LV)
+example_signFunction<- function(LV, m, some_constant, some_array){ #LV should contain nonegative min2max sorted elements, m is in 1:length(LV)
     #find m for which sum_m >= some_constant and sum_pm < some_constant (previous m, that is m -1)
-    LV_thr.m<- LV
-    LV_thr.m[ LV < LV[[m]] ] = 0
-    sum_m <- sum(LV_thr.m)
+    array_thr.m<- some_array
+    array_thr.m[ some_array < LV[[m]] ] = 0
+    sum_m <- sum(array_thr.m)
     if (sum_m < some_constant) 
         return(-1)
     if ( (m==1) && ( sum_m >= some_constant) ) #special case, if there is no previous m, condition on m only suffices that m is a solution
         return(0)
-    LV_thr.pm<- LV   #pm <- previous m
-    LV_thr.pm[ LV < LV[[m-1]] ] = 0
-    sum_pm <- sum(LV_thr.pm)
+    array_thr.pm<- some_array  #pm <- previous m
+    array_thr.pm[ some_array < LV[[m-1]] ] = 0
+    sum_pm <- sum(array_thr.pm)
     if ( ( sum_m >= some_constant) && (sum_pm >= some_constant) )  
         return(1)
     if ( ( sum_m >= some_constant) && (sum_pm < some_constant) ) #only possible other outcome if all LV are positive nad LV is sorted 
